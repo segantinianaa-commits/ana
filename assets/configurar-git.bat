23e5a39b60e6eea29253bec0280e9666cffaa25c
Nome do Aluno <email@aluno.com>
2026-08-19 12:14:21 -0300

    first commit

--- assets/configurar-git.bat
+++ assets/configurar-git.bat
@@ -1,5 +1,5 @@
 @echo off
-git config user.name "Nome do Aluno"
-git config user.email "email@aluno.com"
+git config user.name "segantinianaa-commits"
+git config user.email "segantinianaa@gmail.com"
 echo Git configurado com sucesso para este projeto!
 pause
